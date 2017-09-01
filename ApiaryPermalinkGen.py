dfb4827542fb2cb82d293bb585dbb128f9e548d4
Mikk Laos <mikk.laos@gmail.com>
2017-09-01 16:11:55 +0300

    Reveal to world

---
+++ ApiaryPermalinkGen.py
@@ -0,0 +1,128 @@
+import fileinput
+import argparse
+import re
+import errno
+
+import sys
+
+defaultOutputFile = "apiaryWithPermaLinks.txt"
+
+apiNameLineRegex = "^## \w.*"
+apiNameSliceRegex = "(?!##) (?!\w.)"
+oldPermaLinkRegex = "<a.*>"
+
+numPermalinks = 0
+outPutFile = ""
+cmdParser = object
+removeOldPermalinks = False
+
+
+def getMatchedLine(regex, line):
+    m = re.search(regex, line)
+    if m:
+        return m
+    return None
+
+
+def getTokenizedApiName(slicePoint, foundLine):
+    lowerCaseApiName = foundLine.lower()
+    return lowerCaseApiName[3:slicePoint].replace(" ", "-")
+
+
+def getPermaLinkName(foundLine):
+    slicePoint = getMatchedLine(apiNameSliceRegex, foundLine)
+    if slicePoint:
+        slicePoint = slicePoint.start()
+        return getTokenizedApiName(slicePoint, foundLine)
+
+
+def getLineWithPermalink(line):
+    foundLine = getMatchedLine(apiNameLineRegex, line)
+    s = ""
+    if foundLine:
+        s = getPermaLinkName(foundLine.string)
+        return "<a name=\"" + s + "\"/>"
+
+
+def isOldPermalink(line):
+    foundPermalink = getMatchedLine(oldPermaLinkRegex, line)
+    if foundPermalink:
+        return True
+    return False
+
+
+def writePermaLinkToFile(file, line, permaLink):
+    s = line + permaLink + "\n"
+    file.write(s)
+
+
+def checkIfReadable(file):
+    try:
+        textfile = open(file, 'r')
+        textfile.close()
+    except IOError as e:
+        if e.errno == errno.EACCES:
+            print("file exists, but isn't readable")
+        elif e.errno == errno.ENOENT:
+            print("files isn't readable because it isn't there")
+        sys.exit()
+
+
+def writeToFile(inputFile, outFile=defaultOutputFile):
+    with fileinput.FileInput(inputFile) as file, open(outFile, 'w') as new:
+        global numPermalinks
+        for line in file:
+            s = ""
+            permaLinkLine = getLineWithPermalink(line)
+            if permaLinkLine:
+                writePermaLinkToFile(new, line, permaLinkLine)
+                numPermalinks += 1
+            elif removeOldPermalinks and isOldPermalink(line):
+                new.write("")
+            else:
+                new.write(line)
+
+
+def createParser():
+    global cmdParser
+    cmdParser = argparse.ArgumentParser(description='Permalink creator for apiary Blueprint')
+    cmdParser.add_argument('-i', '--input_file', help='File that contains apiary document', required=True)
+    cmdParser.add_argument('-c', action="store_true", help='Clear existing permalinks', required=False)
+    cmdParser.add_argument('-o', '--output_file', help='Output file apiary doument with permalinks', required=False)
+
+
+def getArguments():
+    return cmdParser.parse_args()
+
+
+def checkForClearPermaliks(args):
+    global removeOldPermalinks
+    if args.c:
+        removeOldPermalinks = True
+
+
+def endPrint():
+    print("Wrote: {} permalinks to {}".format(numPermalinks, outPutFile))
+
+
+def main():
+    global outPutFile
+    createParser()
+
+    inputFile = getArguments().input_file
+    outPutFile = getArguments().output_file
+
+    checkIfReadable(inputFile)
+    checkForClearPermaliks(getArguments().c)
+
+    if outPutFile:
+        checkIfReadable(outPutFile)
+        writeToFile(inputFile, outPutFile)
+    else:
+        outPutFile = defaultOutputFile
+        writeToFile(inputFile)
+
+    endPrint()
+
+
+main()
---
+++ Readme.md
@@ -0,0 +1,52 @@
+# Apiary Permalink Generator
+
+Generates permalinks for existing apiary blueprint. Permalinks are generated from existing services in format your-service-name
+
+## Getting Started
+
+Download ApiaryPermalinkGen.py and run
+
+```
+python ApiaryPermalinkGen.py -i "your-file-with-apiary-blueprint"
+```
+
+It will generate new file called apiaryWithPermaLinks.txt in the same dir where the python file is. Generated file will contain blueprint with permalinks. Every permalink will be inserted after service name in format of <a name="your-service-name">
+
+## Options
+- `-c` Clears previous permaliks. Use this option if you already have permalinks in your blueprint
+- `-o` Output file. Allows to specify output file to write blueprint.
+
+## Examples
+Generate  with permaliks:
+```
+python ApiaryPermalinkGen.py -i "your-file-with-apiary-blueprint"
+```
+Clean previous permalinks and generate:
+```
+python ApiaryPermalinkGen.py -i -c "your-file-with-apiary-blueprint"
+```
+Clean previous permalinks specify output file and generate:
+```
+python ApiaryPermalinkGen.py -i "your-file-with-apiary-blueprint" -o "your-output-file.txt" -c
+```
+
+
+### Prerequisites
+
+You need Python 3.3 or later to run Apiary Permalink Generator.  You can have multiple Python
+versions (2.x and 3.x) installed on the same system without problems.
+
+In Ubuntu, Mint and Debian you can install Python 3 like this:
+
+    $ sudo apt-get install python3 python3-pip
+
+For other Linux flavors, OS X and Windows, packages are available at
+
+  http://www.python.org/getit/
+
+## Author
+* Mikk Laos
+
+## License
+
+This project is licensed under the MIT License. This product comes with no warranty.
